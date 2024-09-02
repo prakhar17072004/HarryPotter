5770cdb29e2e43bb1005fc565d4e71a02ec43239
prakhar17072004 <pmankur2004@gmail.com>
2024-09-02 22:36:42 +0530

    new file is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Image from "next/image";
 
+import Navbar from "@/component/Navbar";
+
+
 export default function Home() {
   return (
     <div className="bg-red-500 text-center">
-      Prakhar mishra
+      <Navbar/>
+    
+      
     </div>
   );
 }
---
+++ src/component/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Books from "@/Pages/Books";
+import Character from "@/Pages/Character";
+import Houses from "@/Pages/Houses";
+import Link from "next/link";
+function Navbar() {
+  return (
+    <div>
+      <Link to="">Books</Link>
+      
+    </div>
+  );
+}
+
+export default Navbar;
